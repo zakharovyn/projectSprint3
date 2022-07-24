827156a813d5f7daa08de219e80a154a6f7b03c1
Zakharov Yan <banjumingray@bk.ru>
2022-07-24 16:45:05 +0700

    Удаление связи, Outlet кнопки и блока кода viewDidLoad()

--- projectSprint3/ViewController.swift
+++ projectSprint3/ViewController.swift
@@ -1,29 +1,16 @@
-//
 //  ViewController.swift
 //  projectSprint3
-//
-//  Created by Ян Захаров on 23.07.2022.
-//
+//  Под IPhone 11
 
 import UIKit
 
 class ViewController: UIViewController {
+    var count: Int = 0
     
     @IBOutlet weak var labelCount: UILabel!
     
-    @IBOutlet weak var buttonCount: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    var count: Int = 0
-
     @IBAction func changeCount(_ sender: Any) {
         count += 1
         labelCount.text = "Значение счётчика: \(count)"
     }
-    
 }
-
